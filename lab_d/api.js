9fd64df23662047711adac7094f64a2c06b86000
Patryk Wróblewski <wp53906@zut.edu.pl>
2024-11-24 19:56:20 +0100

    lab_d

--- lab_d/api.js
+++ lab_d/api.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function getWeather(){
         let req = new XMLHttpRequest();
-
+		api.openweathermap.org/data/2.5/forecast?q=Szczecin&appid=dfbabb73a542884e5ecb35ba2bee30be
         req.open("GET", `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_key}&units=metric`, true);
         req.addEventListener("load", function(event) {
 
